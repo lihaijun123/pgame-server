97b7b21f3a772d727081b571b3458ac8f8578865
lihaijun123 <496174528@qq.com>
2017-03-30 22:56:53 +0800

    222

--- src/main/java/com/focus3d/pgame/server/GameServer.java
+++ src/main/java/com/focus3d/pgame/server/GameServer.java
@@ -1,12 +1,18 @@
 package com.focus3d.pgame.server;
 
+import com.focus3d.pgame.server.handler.GameServerHandler;
+
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 /**
  * websocket server
@@ -30,8 +36,11 @@ public class GameServer {
 			.childHandler(new ChannelInitializer<SocketChannel>() {
 				@Override
 				protected void initChannel(SocketChannel ch) throws Exception {
-					
-				}
+					ChannelPipeline pipeline = ch.pipeline();
+					pipeline.addLast("http-codec", new HttpServerCodec());
+					pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
+					pipeline.addLast("http-chunked", new ChunkedWriteHandler());
+					pipeline.addLast("handler", new GameServerHandler());				}
 			});
 			Channel ch = b.bind(port).sync().channel();
 			System.out.println("web socket server started at port " + port + ".");
@@ -42,4 +51,16 @@ public class GameServer {
 			workerGroup.shutdownGracefully();
 		}
 	}
+	
+	public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        new GameServer().run(port);
+    }
 }
